0f1c1f26fe5279bc9999bef0d7d709617e70baaf
Orion Letizi <oletizi@mac.com>
2016-09-29 12:17:19 -0700

    added git pull to webhook startup

--- bin/webhook.sh
+++ bin/webhook.sh
@@ -7,6 +7,7 @@ mydir=`pwd`
 cd $mydir/..
 projdir=`pwd`
 
+git pull
 mvn -DskipTests clean package
 
 java -cp $projdir/target/avi-docs-snarfer-1.1-jar-with-dependencies.jar com.avinetworks.docs.web.PushHandler
